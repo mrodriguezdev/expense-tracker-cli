2cce92603d7c930d1bcbaf8ea4181a238947e5a7
mrodriguezdev <marioguzman_99@hotmail.com>
2024-08-31 17:15:37 -0600

    agregando clase principal de la aplicación y configuración inicial del comando CLI
    - Añadida la clase  como el punto de entrada principal de la aplicación.
    - Creada la clase  con la configuración de la interfaz de línea de comandos (CLI).
    - La CLI incluye una descripción, versión y subcomandos disponibles para gestionar los gastos.

---
+++ src/main/java/com/mrodriguezdev/command/ExpenseTrackerCliCommand.java
@@ -0,0 +1,37 @@
+package com.mrodriguezdev.command;
+
+import picocli.CommandLine;
+
+@CommandLine.Command(
+        name = "expense-tracker-cli",
+        version = "1.0.0",
+        mixinStandardHelpOptions = true,
+        requiredOptionMarker = '*',
+        description = "Expense Tracker CLI es una herramienta completa para gestionar tus gastos de manera eficiente. " +
+                "Permite agregar, actualizar, eliminar y listar tus gastos, así como generar resúmenes para un seguimiento detallado de tus finanzas personales. " +
+                "Mantén tus gastos organizados y controla tu presupuesto con facilidad.",
+        header = "Expense Tracker CLI",
+        optionListHeading = "%nOpciones disponibles:%n",
+        footerHeading = "%nInformación Adicional:%n",
+        footer = "%nDesarrollado por mrodriguezdev - © 2024"
+)
+public class ExpenseTrackerCliCommand implements Runnable {
+    public static void main(String[] args) {
+        int status = new CommandLine(new ExpenseTrackerCliCommand())
+                .execute(args);
+        System.exit(status);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("[expense-tracker-cli] Bienvenido a Expense Tracker CLI.");
+        System.out.println("Esta herramienta te permite gestionar y hacer un seguimiento de tus gastos de manera eficiente.");
+        System.out.println("Utiliza los siguientes subcomandos para empezar:");
+        System.out.println("  - add   : Añade un nuevo gasto.");
+        System.out.println("  - update: Actualiza un gasto existente.");
+        System.out.println("  - delete: Elimina un gasto.");
+        System.out.println("  - list  : Lista todos los gastos.");
+        System.out.println("  - summary: Muestra un resumen de tus gastos.");
+        System.out.println("Para obtener más información sobre cada comando, utiliza la opción '-h' o '--help'.");
+    }
+}
